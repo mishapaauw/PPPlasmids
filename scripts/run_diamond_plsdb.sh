a4cc35bea485cf135f7273c39df689a3457774ff
mishapaauw <m.m.paauw@uva.nl>
2026-03-16 17:15:24 +0100

    add plsdb analysis

--- scripts/run_diamond_plsdb.sh
+++ scripts/run_diamond_plsdb.sh
@@ -1,12 +1,12 @@
 diamond blastp \
-	--query '/scratch/misha/plsdb/plsdb_mini_clean.fasta' \
+	--query '/scratch/misha/plsdb/plsdb_clean.fasta' \
 	--db 'diamond_db/proteins' \
 	--out 'output/plsdb_diamond_out.tsv' \
 	--outfmt  6 qseqid sseqid pident length qlen slen qcovhsp evalue bitscore \
 	--evalue  '1e-5' \
 	--query-cover 50 \
 	--id      30 \
-	--threads 12 \
+	--threads 32 \
 	--max-target-seqs 5000 \
 	--more-sensitive \
 	--no-self-hits
